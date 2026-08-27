13915381ba7b92caf3994189e656fa6689343507
divyanshugupta123 <91783202+divyanshugupta123@users.noreply.github.com>
2026-08-27 10:38:41 +0530

    Time: 0 ms (100%), Space: 48.2 MB (31.96%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,17 +1,18 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        return new int[]{findFirst(nums,target),findLast(nums,target)};
+        return new int[]{first(nums,target),last(nums,target)};
     }
-    public int findFirst(int[] arr,int target){
-        int low = 0;
-        int high = arr.length-1;
+    public int last(int[] nums,int target){
         int idx = -1;
+        int low = 0;
+        int high = nums.length - 1;
         while(low<=high){
             int mid = low + (high-low)/2;
-            if(arr[mid]==target){
+            if(nums[mid]==target){
                 idx = mid;
-                high = mid - 1;
-            }else if(arr[mid]>target){
+                low = mid + 1;
+            }
+            else if(nums[mid]>target){
                 high = mid - 1;
             }else{
                 low = mid + 1;
@@ -19,16 +20,17 @@ class Solution {
         }
         return idx;
     }
-    public int findLast(int[] arr,int target){
-        int low = 0;
-        int high = arr.length-1;
+    public int first(int[] nums,int target){
         int idx = -1;
+        int low = 0;
+        int high = nums.length - 1;
         while(low<=high){
             int mid = low + (high-low)/2;
-            if(arr[mid]==target){
+            if(nums[mid]==target){
                 idx = mid;
-                low = mid + 1;
-            }else if(arr[mid]>target){
+                high = mid - 1;
+            }
+            else if(nums[mid]>target){
                 high = mid - 1;
             }else{
                 low = mid + 1;
